1996f1c469a5b93cd66a5a260bf33b35268472b1
grusad <gillberg.alexander@gmail.com>
2021-07-18 16:18:43 +0200

    HEJSAN

--- src/Test.gd
+++ src/Test.gd
@@ -5,7 +5,7 @@ onready var world_env = get_node("WorldEnvironment")
 
 func _ready():
 	Globals.connect("light_factor", self, "on_light_factor_changed")
-	
+	world_env.environment.ambient_light_energy = 1
 
 func on_light_factor_changed(amount):
 	if amount == 1:
